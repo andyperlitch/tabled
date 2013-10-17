c93279dc9510f0e89909c06eb34df53829ad858a
Andy Perlitch <andyperlitch@gmail.com>
2013-10-17 13:39:41 -0700

    0.3.0

--- standalone/tabled.js
+++ standalone/tabled.js
@@ -97,7 +97,7 @@ var Tabled = BaseView.extend({
         // if ( !(this.collection instanceof Backbone.Collection) ) throw new Error("Tabled must be provided with a backbone collection as its data");
         
         // Config object
-        this.config = new ConfigModel(this.options);
+        this.config = new ConfigModel(options);
 
         // Columns
         this.columns = new Columns(this.config.get("columns"),{config: this.config});
@@ -389,7 +389,7 @@ var Tabled = BaseView.extend({
 });
 
 exports = module.exports = Tabled
-},{"./lib/Column":3,"./lib/Thead":4,"./lib/Scroller":5,"./lib/Tbody":6,"underscore":"XqG2h/","backbone":"7/u9/g","bassview":7}],8:[function(require,module,exports){
+},{"./lib/Column":3,"./lib/Thead":4,"./lib/Tbody":5,"./lib/Scroller":6,"underscore":"XqG2h/","backbone":"7/u9/g","bassview":7}],8:[function(require,module,exports){
 function like(term, value, computedValue, row) {
     var
     term = term.toLowerCase(),
@@ -975,7 +975,96 @@ var Thead = BaseView.extend({
     
 });
 exports = module.exports = Thead;
-},{"underscore":"XqG2h/","bassview":7}],5:[function(require,module,exports){
+},{"bassview":7,"underscore":"XqG2h/"}],5:[function(require,module,exports){
+var BaseView = require('bassview');
+
+var Trow = BaseView.extend({
+    
+    className: 'tr',
+    
+    initialize: function() {
+        this.listenTo(this.model, "change", this.render);
+    },
+    
+    render: function() {
+        var html = '';
+        this.collection.each(function(column){
+            var id = column.get('id');
+            var width = column.get('width');
+            var formatted = column.getFormatted(this.model);
+            html += '<div class="td col-' + id + '" style="width:' + width + 'px"><div class="cell-inner">' + formatted + '</div></div>';
+        }, this);
+        this.$el.html(html);
+        return this;
+    }
+});
+
+
+
+var Tbody = BaseView.extend({
+    
+    initialize: function(options) {
+        this.columns = options.columns;
+        this.config = this.columns.config;
+        this.listenTo(this.collection, "reset sort sync", this.render);
+        // this.listenTo(this.collection, "update", this.render); // commented out because 'sort' event gets called anyway on update
+        this.listenTo(this.config, "update change:offset", this.render);
+        this.listenTo(this.columns, "change:width", this.adjustColumnWidth );
+    },
+    
+    adjustColumnWidth: function(model, newWidth, options){
+        this.$('.td.col-'+model.get("id")).width(newWidth);
+    },
+    
+    // Renders the visible rows given the current offset and max_rows
+    // properties on the config object.
+    render: function() {
+        this.$el.empty();
+        this.trigger("clean_up");
+        var rows_to_render = this.config.getVisibleRows();
+        if (rows_to_render === false) return;
+        
+        rows_to_render.forEach(function(row){
+            var rowView = new Trow({
+                model: row,
+                collection: this.columns
+            });
+            this.$el.append( rowView.render().el );
+            rowView.listenTo(this, "clean_up", rowView.remove);
+        }, this);
+        this.trigger('rendered');
+        return this;
+    },
+    
+    events: {
+        "wheel": "onMouseWheel",
+        "mousewheel": "onMouseWheel"
+    },
+    
+    onMouseWheel: function(evt) {
+        // BigInteger
+        var self = this;
+        
+        // normalize webkit/firefox scroll values
+        var deltaY = -evt.originalEvent.wheelDeltaY || evt.originalEvent.deltaY * 100;
+        var movement = Math.round(deltaY / 100);
+        if (isNaN(movement)) return;
+        var origOffset = this.config.get("offset");
+        var limit = this.config.get("max_rows");
+        var offset = Math.min( this.collection.length - limit, Math.max( 0, origOffset + movement));
+        
+        this.config.set({"offset": offset}, {validate: true} );
+        
+        // Prevent default only when necessary
+        if (offset > 0 && offset < this.collection.length - limit) {
+            evt.preventDefault();
+            evt.originalEvent.preventDefault();
+        }
+    }
+    
+});
+exports = module.exports = Tbody;
+},{"bassview":7}],6:[function(require,module,exports){
 var BaseView = require('bassview');
 var Scroller = BaseView.extend({
     
@@ -1046,95 +1135,6 @@ var Scroller = BaseView.extend({
 });
 
 exports = module.exports = Scroller
-},{"bassview":7}],6:[function(require,module,exports){
-var BaseView = require('bassview');
-
-var Trow = BaseView.extend({
-    
-    className: 'tr',
-    
-    initialize: function() {
-        this.listenTo(this.model, "change", this.render);
-    },
-    
-    render: function() {
-        var html = '';
-        this.collection.each(function(column){
-            var id = column.get('id');
-            var width = column.get('width');
-            var formatted = column.getFormatted(this.model);
-            html += '<div class="td col-' + id + '" style="width:' + width + 'px"><div class="cell-inner">' + formatted + '</div></div>';
-        }, this);
-        this.$el.html(html);
-        return this;
-    }
-});
-
-
-
-var Tbody = BaseView.extend({
-    
-    initialize: function(options) {
-        this.columns = options.columns;
-        this.config = this.columns.config;
-        this.listenTo(this.collection, "reset sort", this.render);
-        // this.listenTo(this.collection, "update", this.render); // commented out because 'sort' event gets called anyway on update
-        this.listenTo(this.config, "update change:offset", this.render);
-        this.listenTo(this.columns, "change:width", this.adjustColumnWidth );
-    },
-    
-    adjustColumnWidth: function(model, newWidth, options){
-        this.$('.td.col-'+model.get("id")).width(newWidth);
-    },
-    
-    // Renders the visible rows given the current offset and max_rows
-    // properties on the config object.
-    render: function() {
-        this.$el.empty();
-        this.trigger("clean_up");
-        var rows_to_render = this.config.getVisibleRows();
-        if (rows_to_render === false) return;
-        
-        rows_to_render.forEach(function(row){
-            var rowView = new Trow({
-                model: row,
-                collection: this.columns
-            });
-            this.$el.append( rowView.render().el );
-            rowView.listenTo(this, "clean_up", rowView.remove);
-        }, this);
-        this.trigger('rendered');
-        return this;
-    },
-    
-    events: {
-        "wheel": "onMouseWheel",
-        "mousewheel": "onMouseWheel"
-    },
-    
-    onMouseWheel: function(evt) {
-        // BigInteger
-        var self = this;
-        
-        // normalize webkit/firefox scroll values
-        var deltaY = -evt.originalEvent.wheelDeltaY || evt.originalEvent.deltaY * 100;
-        var movement = Math.round(deltaY / 100);
-        if (isNaN(movement)) return;
-        var origOffset = this.config.get("offset");
-        var limit = this.config.get("max_rows");
-        var offset = Math.min( this.collection.length - limit, Math.max( 0, origOffset + movement));
-        
-        this.config.set({"offset": offset}, {validate: true} );
-        
-        // Prevent default only when necessary
-        if (offset > 0 && offset < this.collection.length - limit) {
-            evt.preventDefault();
-            evt.originalEvent.preventDefault();
-        }
-    }
-    
-});
-exports = module.exports = Tbody;
 },{"bassview":7}],7:[function(require,module,exports){
 (function(){var _ = require('underscore'), Backbone = require('backbone');
 var BassView = Backbone.View.extend({
